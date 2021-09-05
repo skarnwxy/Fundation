003804f631ec8e3d56d3bb5cd98f7d5b4362e899
skarnwxy <49404893+skarnwxy@users.noreply.github.com>
2021-09-05 18:16:45 +0800

    <Fundation项目><STL迭代器>

---
+++ STLIterator/STLIterator.cpp
@@ -0,0 +1,21 @@
+﻿
+#include <iostream>
+#include <vector>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+	vector<int>intVect(5);
+
+	vector<int>::iterator out = intVect.begin();
+
+	std::cout << "Vect: ";
+	vector <int>::iterator it = intVect.begin();
+	while (it != intVect.end())
+	{
+		std::cout << *it++ << std::endl;
+	}
+	std::endl;
+
+}
+
