85a166432e57569ef2648a3aae062500e1742da1
skarnwxy <skarnwxy@sina.com>
2022-06-26 15:22:00 +0800

    <Foundation><获取快捷键 — 键盘消息处理>

---
+++ GetShortcut/GetShortcut.cpp
@@ -0,0 +1,31 @@
+﻿/** 
+  * Opencascade Skarn Copyright (C) 2022
+  * @file     GetShortcut.h                                                       
+  * @brief    Windows平台全局捕获键盘消息                                                    
+  * @author   debugSarn                                                     
+  * @date     2022-03-30  
+  * @note     
+  * @see      
+ */
+
+#include<iostream>
+#include<windows.h>
+
+#define KEY_Down(vKey) ((GetAsyncKeyState(vKey) & 0x8000) ? 1:0)
+using namespace std;
+
+int main() {
+	while (1) {
+		if (KEY_Down(VK_SHIFT) && KEY_Down('A')) {
+			cout << " Shift + A";
+			Sleep(150);
+		}
+
+		if (KEY_Down(VK_SHIFT) && KEY_Down('B')) {
+			cout << " Shift + B";
+			Sleep(150);
+		}
+	}
+	return 0;
+}
+
